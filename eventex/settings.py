cc0cc70472fbf6696f932feef603d4db676aa36c
sergio.sousa <sergio.sousa@armazemparaiba.com.br>
2018-06-14 22:43:11 -0300

    teste

--- eventex/settings.py
+++ eventex/settings.py
@@ -27,8 +27,6 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=False, cast=bool)
 
-#ALLOWED_HOSTS = ['*']
-#ALLOWED_HOSTS = ['127.0.0.1','.localhost','.herokuapp.com']
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 
 # Application definition
