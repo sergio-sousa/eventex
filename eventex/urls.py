888f5746e8ece0bd32d76d0348613383332b2fcf
sergio.sousa <sergio.sousa@armazemparaiba.com.br>
2018-07-01 13:52:46 -0300

    Subscription

--- eventex/urls.py
+++ eventex/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 import eventex.core.views
 
+
 urlpatterns = [
     path('',eventex.core.views.home),
     path('admin/', admin.site.urls),
