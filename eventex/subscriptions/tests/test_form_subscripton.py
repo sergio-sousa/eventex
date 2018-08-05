e53d76cd17f5622e8fd854968167a8f18877eabd
sergio.sousa <sergio.sousa@armazemparaiba.com.br>
2018-08-05 12:57:24 -0300

    patroniza nomes

--- eventex/subscriptions/tests/test_form_subscripton.py
+++ eventex/subscriptions/tests/test_form_subscripton.py
@@ -13,11 +13,16 @@ class SubscriptionFormTest(TestCase):
         form = self.make_validated_form(cpf = 'ABCD5678901')
         self.assertFormErrorCode(form, 'cpf', 'digits')
         
-
     def test_cpf_has_11_digit(self):
         """CPF must have 11 digits """
         form = self.make_validated_form(cpf='1234')
         self.assertFormErrorCode(form, 'cpf', 'length')
+
+    def test_name_must_be_capitalized(self):
+        """Name must be capitalized. """
+        form = self.make_validated_form(name='HENRIQUE bastos')
+        self.assertEqual('Henrique Bastos', form.cleaned_data['name'])
+        
 
     def assertFormErrorCode(self, form, field, code):
         errors = form.errors.as_data()
