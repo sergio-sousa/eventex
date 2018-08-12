7052f0c3b57b4d87cec9789f18923bf5d0c0cdcc
sergio.sousa <sergio.sousa@armazemparaiba.com.br>
2018-08-11 21:15:05 -0300

    Speaker

--- eventex/urls.py
+++ eventex/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 from eventex.core.views import home
+from eventex.core.views import speaker_detail
 
 urlpatterns = [
     path('',home, name='home'),
     path('inscricao/', include('eventex.subscriptions.urls')),
+    path('palestrantes/<slug:slug>/', speaker_detail, name='speaker_detail'),
     path('admin/', admin.site.urls),
 ]
