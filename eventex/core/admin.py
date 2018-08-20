1c1f8118ef6d8494369154314a2b1c9a73354729
sergio-sousa <sergio.sousa@armazemparaiba.com.br>
2018-08-19 21:08:12 -0300

    Managers

--- eventex/core/admin.py
+++ eventex/core/admin.py
@@ -11,7 +11,7 @@ class ContactInLine(admin.TabularInline):
 class SpeakerModelAdmin(admin.ModelAdmin):
     inlines = [ContactInLine]
     prepopulated_fields = {'slug': ('name',)}
-    list_display = ['name', 'photo_img', 'website_link']
+    list_display = ['name', 'photo_img', 'website_link','email', 'phone']
 
     def website_link(self, obj):
         return format_html('<a href="{0}">{0}</a>', obj.website)
@@ -21,7 +21,18 @@ class SpeakerModelAdmin(admin.ModelAdmin):
     def photo_img(self, obj):
         return format_html('<img width="32px" src="{}" />', obj.photo)
 
+    photo_img.allow_tags = True
     photo_img.short_description = 'foto'
+
+    def email(self):
+        return obj.contact_set.emails()
+            
+    email.short_description = 'e-mail'
+
+    def phone(self):
+        return obj.contact_set.phones()
+               
+    phone.short_description = 'telefone'
 
 
 admin.site.register(Speaker, SpeakerModelAdmin)
--- eventex/core/models.py
+++ eventex/core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.shortcuts import resolve_url as r
+from eventex.core.managers import KindQuerySet
+from eventex.core.managers import PeriodManager
 
 
 class Speaker(models.Model):
@@ -31,6 +33,8 @@ class Contact(models.Model):
     kind = models.CharField('tipo',max_length=1, choices=KINDS)
     value = models.CharField('valor',max_length=255)
 
+    objects = KindQuerySet.as_manager()
+    
     class Meta:
         verbose_name = 'contato'
         verbose_name_plural = 'contatos'
@@ -43,6 +47,8 @@ class Talk(models.Model):
     start = models.TimeField('início', blank=True, null=True)
     description = models.TextField('descrição', blank=True)
     speakers = models.ManyToManyField('Speaker', verbose_name = 'palestrantes', blank=True)
+
+    objects = PeriodManager()
 
     class Meta:
         verbose_name_plural = 'palestras'
